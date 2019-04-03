183c374640bd7f63f4e03877958c1776115ab14e
Carls13 <charlesshb98@gmail.com>
2019-04-03 18:42:11 -0400

    Updates

---
+++ precache-manifest.297e16d286ff8c7019084cc5a6b89a08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5af606808f9584e89704",
+    "url": "/smart-brain-frontend/static/js/runtime~main.5af60680.js"
+  },
+  {
+    "revision": "a0caa8f02bb9b6a0e740",
+    "url": "/smart-brain-frontend/static/js/main.a0caa8f0.chunk.js"
+  },
+  {
+    "revision": "0306c41e6037523b340b",
+    "url": "/smart-brain-frontend/static/js/1.0306c41e.chunk.js"
+  },
+  {
+    "revision": "a0caa8f02bb9b6a0e740",
+    "url": "/smart-brain-frontend/static/css/main.6bc97532.chunk.css"
+  },
+  {
+    "revision": "0306c41e6037523b340b",
+    "url": "/smart-brain-frontend/static/css/1.04a72f82.chunk.css"
+  },
+  {
+    "revision": "5b6ad5e3db6d8cbd2c7a11958bb53c2c",
+    "url": "/smart-brain-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/smart-brain/precache-manifest.79f86130f2aa7a61614750faba0aec57.js"
+  "/smart-brain-frontend/precache-manifest.297e16d286ff8c7019084cc5a6b89a08.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/smart-brain/index.html", {
+workbox.routing.registerNavigationRoute("/smart-brain-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
